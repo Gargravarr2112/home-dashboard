a4f1ac1c17fb2ad8fc4428dea22b1649eedd52dd
Rob Johnson <quantumparticleresearch@gmail.com>
2019-09-01 15:41:03 +0100

    Added state in dummy to test toggling

--- src/dummy.py
+++ src/dummy.py
@@ -4,13 +4,15 @@ Fake classes to avoid RPi dependencies
 class Adafruit_DHT:
  DHT11 = 11
  def read_retry(sensor, pin):
-   return(0, 0)
+   return(50, 20)
 
 class Energenie:
   value = False
   def __init__(self, channel):
     pass
   def on(self):
+    self.value = True
     return True
   def off(self):
+    self.value = False
     return False
