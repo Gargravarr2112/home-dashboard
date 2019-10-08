a8e951a3cdab050fe1492fa91ba3c06725662a43
Rob Johnson <quantumparticleresearch@gmail.com>
2019-10-08 16:34:02 +0100

    Checking for rain tomorrow if none found today. Added day start and end to determine what graphic to use.

--- src/weather.py
+++ src/weather.py
@@ -11,12 +11,18 @@ FORECAST_URL = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json
 METOFFICE_WEATHER_KEY = {0: "Clear night", 1: "Sunny day", 2: "Partly cloudy (night)", 3: "Partly cloudy (day)", 5: "Mist", 6: "Fog", 7: "Cloudy", 8: "Overcast", 9: "Light rain shower (night)", 10: "Light rain shower (day)", 11: "Drizzle", 12: "Light rain", 13: "Heavy rain shower (night)", 14: "Heavy rain shower (day)", 15: "Heavy rain", 16: "Sleet shower (night)", 17: "Sleet shower (day)", 18: "Sleet", 19: "Hail shower (night)", 20: "Hail shower (day)", 21: "Hail", 22: "Light snow shower (night)", 23: "Light snow shower (day)", 24: "Light snow", 25: "Heavy snow shower (night)", 26: "Heavy snow shower (day)", 27: "Heavy snow", 28: "Thunder shower (night)", 29: "Thunder shower (day)", 30: "Thunder"}
 WEATHER_IMAGE_KEY = {0: "night-clear", 1: "day-clear", 2: "night-cloud", 3: "day-cloud", 5: "fog", 6: "fog", 7: "cloudy", 8: "cloudy", 9: "night-rain", 10: "day-rain", 11: "$t-rain", 12: "$t-rain", 13: "$t-rain", 14: "heavy-rain", 15: "heavy-rain", 16: "snow", 17: "snow", 18: "snow", 19: "snow", 20: "snow", 21: "snow", 22: "snow", 23: "snow", 24: "snow", 25: "snow", 26: "snow", 27: "snow", 28: "thunder", 29: "thunder", 30: "thunder"}
 RAIN_THRESHHOLD = 50 #%
+DAY_START = 7
+DAY_END = 19
 config = {}
 
 with open('weather.json', 'r') as configFile:
-	logger.debug("Loading config file")
-	config = json.load(configFile)
-	logger.debug("Loaded config file")
+	try:
+		logger.debug("Loading config file")
+		config = json.load(configFile)
+		logger.debug("Loaded config file")
+	except Exception as e:
+		logger.error("Failed to load weather.json:")
+		logger.error(e)
 
 API_KEY = config['APIKey']
 
@@ -29,8 +35,17 @@ def getWeather():
 		return
 	weatherData = weatherRequest.json()
 	today = weatherData['SiteRep']['DV']['Location']['Period'][0]
-	date = datetime.datetime.strptime(today['value'], '%Y-%m-%dZ')
-	reports = today['Rep']
+	rain = checkForRain(today)
+	if rain[1] == 0: #No rain today, so check tomorrow too
+		logger.debug("No rain predicted today, checking tomorrow")
+		tomorrow = weatherData['SiteRep']['DV']['Location']['Period'][1]
+		rain = checkForRain(tomorrow)
+	nextThreeHours = today['Rep'][0]
+	return (nextThreeHours, rain)
+
+def checkForRain(dayForecast):
+	date = datetime.datetime.strptime(dayForecast['value'], '%Y-%m-%dZ')
+	reports = dayForecast['Rep']
 	rain = (datetime.datetime.now(), 0)
 	for report in reports:
 		interval = datetime.timedelta(minutes=int(report['$'])) #$ in this instance being the time since midnight in minutes
@@ -38,10 +53,15 @@ def getWeather():
 		rainProbability = int(report['Pp'])
 		if rainProbability > RAIN_THRESHHOLD:
 			rain = (reportTime, rainProbability)
-	nextThreeHours = reports[0]
-	return (nextThreeHours, rain)
+	return rain
 
 def getWeatherType(typeCode):
 	weatherName = METOFFICE_WEATHER_KEY[typeCode]
-	weatherImage = "img/{0}.png".format(WEATHER_IMAGE_KEY[typeCode].replace('$t', 'night')) #TODO: pass in day or night and select accordingly
+	hour = datetime.datetime.now().hour
+	isDay = hour > DAY_START and hour < DAY_END
+	if isDay:
+		weatherTime = 'day'
+	else:
+		weatherTime = 'night'
+	weatherImage = "img/{0}.png".format(WEATHER_IMAGE_KEY[typeCode].replace('$t', weatherTime))
 	return (weatherName, weatherImage)
