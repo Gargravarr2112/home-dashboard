48e1a580e501846eae9931382236104a4d6d85a2
Rob Johnson <quantumparticleresearch@gmail.com>
2020-02-05 19:04:34 +0000

    Added power cost calculation

--- src/power.py
+++ src/power.py
@@ -9,6 +9,8 @@ logger = logging.getLogger(__name__)
 
 ENERGYHIVE_API = "https://www.energyhive.com/mobile_proxy/getEnergy"
 ENERGYHIVE_API_TOKEN = ""
+PENCE_PER_KWH = 14.626
+STANDING_CHARGE = 14.416
 
 FAN_CHANNEL = 1
 HEATER_CHANNEL = 2
@@ -27,24 +29,39 @@ def getYesterdayPowerUse():
 		powerUse = requests.get(ENERGYHIVE_API, requestParameters)
 		if powerUse.status_code != 200:
 			logger.error("API returned HTTP %d", powerUse.status_code)
-			return 0
-		kWh = powerUse.json()['sum']
-		return float(kWh)
+			return (0, 0)
+		kWh = float(powerUse.json()['sum'])
+		costP = (kWh * PENCE_PER_KWH) + STANDING_CHARGE
+		return (kWh, costP)
 	except Exception as e:
 		logger.error("Failed to access EnergyHive API")
 		logger.error(e)
-		return False
+		return (0, 0)
 
 fan = Energenie(FAN_CHANNEL)
 heater = Energenie(HEATER_CHANNEL)
 
 def toggleFan(event=None):
 	logger.debug("Fan toggled")
-	return toggleSocket(fan)
+	if event:
+		button = event.GetEventObject()
+		if toggleSocket(fan):
+			button.SetBackgroundColour('#8AE234')
+		else:
+			button.SetBackgroundColour('#EF2929')
+	else:
+		return toggleSocket(fan)
 
 def toggleHeater(event=None):
 	logger.debug("Heater toggled")
-	return toggleSocket(heater)
+	if event:
+		button = event.GetEventObject()
+		if toggleSocket(heater):
+			button.SetBackgroundColour('#8AE234')
+		else:
+			button.SetBackgroundColour('#EF2929')
+	else:
+		return toggleSocket(heater)
 
 def toggleSocket(socket):
 	if socket.value:
